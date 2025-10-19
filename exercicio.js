5eaecb5bf63c748eee6cd04627b76163a281f092
Julia Melo <estefhanecruz@gmail.com>
2025-10-18 22:40:42 -0300

    first commit

---
+++ exercicio.js
@@ -0,0 +1,14 @@
+//Exercício
+/* Crie um arquivo chamado 'calculator.js
+Escreva uma  função 'add' que recebe dois números como parâmetros e retorna a soma deles
+imprima o resultado da soma dos dois números de exemplo usando 'console.log' */
+
+function add(num1, num2){
+    soma = num1+num2
+    console.log(soma);
+}
+add(120, 63);
+
+
+
+
---
+++ index.js
@@ -0,0 +1,29 @@
+import odd from 'odd';
+const oddNumbers = odd([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+console.log(oddNumbers);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log('hello world!');
+// function mostrarNome(nome){
+//     console.log(`O nome do cliente é ${nome}`);
+// }
+// mostrarNome("Julia");
+
